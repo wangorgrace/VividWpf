dab0a5cf6e143e6aaf5692adce0c3dad678769c6
wangorgrace <1493736851@qq,com>
2022-05-29 17:33:19 +0800

    [VividWpf.UI] Modify the TextBox control template and Add Converter and helper class for it.

---
+++ VividWpf.UI/Controls/Utils/TextBoxHelper.cs
@@ -0,0 +1,29 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows;
+
+namespace VividWpf.UI.Controls.Utils
+{
+    /// <summary>
+    /// The helper class of the TextBox
+    /// </summary>
+    public class TextBoxHelper
+    {
+        public static string GetHitText(DependencyObject obj)
+        {
+            return (string)obj.GetValue(HitTextProperty);
+        }
+
+        public static void SetHitText(DependencyObject obj, string value)
+        {
+            obj.SetValue(HitTextProperty, value);
+        }
+
+        // Using a DependencyProperty as the backing store for HitText.  This enables animation, styling, binding, etc...
+        public static readonly DependencyProperty HitTextProperty =
+            DependencyProperty.RegisterAttached("HitText", typeof(string), typeof(TextBoxHelper), new PropertyMetadata(string.Empty));
+    }
+}
---
+++ VividWpf.UI/Converter/String2BoolConverter.cs
@@ -0,0 +1,20 @@
+﻿using System;
+using System.Globalization;
+using System.Windows.Data;
+
+namespace VividWpf.UI.Converter
+{
+    [ValueConversion(typeof(string),typeof(bool))]
+    public class String2BoolConverter : IValueConverter
+    {
+        public object Convert(object value, Type targetType, object parameter, CultureInfo culture)
+        {
+            return !string.IsNullOrWhiteSpace((string)value);
+        }
+
+        public object ConvertBack(object value, Type targetType, object parameter, CultureInfo culture)
+        {
+            return null;
+        }
+    }
+}
