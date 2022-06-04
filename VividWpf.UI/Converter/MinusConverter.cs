acc18f7995a99b6d82f88029b55ff307423d5cf8
wangorgrace <1493736851@qq,com>
2022-06-04 15:16:38 +0800

    [VividWpf] Finish the first ToggleButton Style.

---
+++ VividWpf.UI/Converter/MinusConverter.cs
@@ -0,0 +1,26 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Globalization;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Data;
+
+namespace VividWpf.UI.Converter
+{
+    /// <summary>
+    /// Minus the value with the parameter
+    /// </summary>
+    public class MinusConverter : IValueConverter
+    {
+        public object Convert(object value, Type targetType, object parameter, CultureInfo culture)
+        {
+            return (double)value - double.Parse((string)(parameter ?? "0"));
+        }
+
+        public object ConvertBack(object value, Type targetType, object parameter, CultureInfo culture)
+        {
+            throw new NotImplementedException();
+        }
+    }
+}
